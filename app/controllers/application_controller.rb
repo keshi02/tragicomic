ab0afc9343988f6194db1532386268f483156dd4
山本翠 <yamamotosui@yamamotomidorinoMacBook-Air.local>
2024-03-20 05:20:37 +0900

    ログアウト機能の実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,10 @@
 class ApplicationController < ActionController::Base
-
+  add_flash_types :success, :danger
   before_action :require_login
 
   private
 
   def not_authenticated
-    redirect_to login_path
+    redirect_to login_path, danger: t('flash_message.require_login')
   end
 end
