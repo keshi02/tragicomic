6bcd8c4f3dd8b4f24cbb28ad372a632e9f282c34
山本翠 <yamamotosui@yamamotomidorinoMacBook-Air.local>
2024-03-21 08:00:16 +0900

    ボタン調整等

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,2 +1,11 @@
 import "@hotwired/turbo-rails"
 import * as bootstrap from "bootstrap/dist/js/bootstrap"
+
+document.addEventListener("DOMContentLoaded", function() {
+    var buttons = document.querySelectorAll(".btn");
+    buttons.forEach(function(button) {
+      button.addEventListener("click", function(event) {
+        event.preventDefault();
+      });
+    });
+  });
